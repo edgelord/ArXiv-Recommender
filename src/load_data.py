69baf02fae7f2e0cde639c8aeda941ecf0b1b6e3
Akshay Mishra <akshay@Akshays-MacBook-Pro.local>
2016-08-12 00:36:01 -0400

    Using feedparser to get entries

--- src/load_data.py
+++ src/load_data.py
@@ -1,11 +1,12 @@
 import urllib
 from urllib.request import urlopen
+import feedparser
 base_url = 'http://export.arxiv.org/api/query?search_query='
 
-def query(term, max_results, heading):
+def query(term, max_results=10, heading='all'):
     """Basic Querying"""
     fetch_url = "{0}{1}:{2}&start=0&max_results={3}"
     print(fetch_url.format(base_url,heading,term,max_results))
     print('http://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=10')
     url = fetch_url.format(base_url,heading,term,max_results)
-    return urlopen(url).read()
+    return feedparser.parse(url).entries
